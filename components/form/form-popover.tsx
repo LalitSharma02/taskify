9f39b266892b8d5834caa538c1106884e5ada38f
Lalit Sharma <lalit.sharma6289@gmail.com>
2024-02-17 21:01:15 +0530

    Added form components

---
+++ components/form/form-popover.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { ElementRef, useRef } from "react";
+// import { toast } from "sonner";
+import { X } from "lucide-react";
+import { useRouter } from "next/navigation";
+
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+  PopoverClose,
+} from "@/components/ui/popover";
+import { useAction } from "@/hooks/use-action";
+import { Button } from "@/components/ui/button";
+import { createBoard } from "@/actions/create-board";
+// import { useProModal } from "@/hooks/use-pro-modal";
+
+import { FormInput } from "./form-input";
+import { FormSubmit } from "./form-submit";
+// import { FormPicker } from "./form-picker";
+
+interface FormPopoverProps {
+  children: React.ReactNode;
+  side?: "left" | "right" | "top" | "bottom";
+  align?: "start" | "center" | "end";
+  sideOffset?: number;
+}
+
+export const FormPopover = ({
+  children,
+  side = "bottom",
+  align,
+  sideOffset = 0,
+}: FormPopoverProps) => {
+  return (
+    <Popover>
+      <PopoverTrigger asChild>{children}</PopoverTrigger>
+      <PopoverContent
+        align={align}
+        className="w-80 pt-3"
+        side={side}
+        sideOffset={sideOffset}
+      >
+        <div className="text-sm font-medium text-center text-neutral-600 pb-4">
+          Create board
+        </div>
+        <PopoverClose asChild>
+          <Button
+            className="h-auto w-auto p-2 absolute top-2 right-2 text-neutral-600"
+            variant="ghost"
+          >
+            <X className="h-4 w-4" />
+          </Button>
+        </PopoverClose>
+      </PopoverContent>
+    </Popover>
+  );
+};
