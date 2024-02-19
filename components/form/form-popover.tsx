113697e7e9b0f161e89b2d1996185537dc5f6bc1
Lalit Sharma <lalit.sharma6289@gmail.com>
2024-02-19 21:32:45 +0530

    Board components created

--- components/form/form-popover.tsx
+++ components/form/form-popover.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { ElementRef, useRef } from "react";
-// import { toast } from "sonner";
+import { toast } from "sonner";
 import { X } from "lucide-react";
 import { useRouter } from "next/navigation";
 
@@ -18,6 +18,7 @@ import { createBoard } from "@/actions/create-board";
 
 import { FormInput } from "./form-input";
 import { FormSubmit } from "./form-submit";
+import { FormPicker } from "./form-picker";
 // import { FormPicker } from "./form-picker";
 
 interface FormPopoverProps {
@@ -33,6 +34,31 @@ export const FormPopover = ({
   align,
   sideOffset = 0,
 }: FormPopoverProps) => {
+
+    const router = useRouter();
+    const closeRef = useRef<ElementRef<"button">>(null);
+
+    const { execute , fieldErrors } = useAction(createBoard , {
+      onSuccess: (data) => {
+        
+        toast.success("Board created!");
+        closeRef.current?.click();
+        router.push(`/board/${data.id}`);
+      } ,
+      onError: (error) => {
+        console.log({ error });
+        toast.error(error);
+      }
+    });
+
+    const onSubmit = (formData: FormData) => {
+      const title = formData.get("title") as string;
+      const image = formData.get("image") as string;
+      
+  
+      execute({ title, image});
+    }
+
   return (
     <Popover>
       <PopoverTrigger asChild>{children}</PopoverTrigger>
@@ -45,7 +71,7 @@ export const FormPopover = ({
         <div className="text-sm font-medium text-center text-neutral-600 pb-4">
           Create board
         </div>
-        <PopoverClose asChild>
+        <PopoverClose ref={closeRef} asChild>
           <Button
             className="h-auto w-auto p-2 absolute top-2 right-2 text-neutral-600"
             variant="ghost"
@@ -53,6 +79,23 @@ export const FormPopover = ({
             <X className="h-4 w-4" />
           </Button>
         </PopoverClose>
+        <form action={onSubmit} className="space-y-4">
+          <div className="space-y-4">
+            <FormPicker 
+            id="image"
+            errors={fieldErrors}
+            />
+            <FormInput 
+            id="title"
+            label="Board title"
+            type="text"
+            errors={fieldErrors}
+            />
+          </div>
+          <FormSubmit className="w-full">
+            Create
+          </FormSubmit>
+        </form>
       </PopoverContent>
     </Popover>
   );
